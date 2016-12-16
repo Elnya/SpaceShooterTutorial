ed8e2a1e43ca752b5b549d04e80b090ae35b380a
Dahye Lee <ldhye9009@gmail.com>
2016-12-16 13:22:32 +0900

    rearrange the lines

--- Assets/Scripts/RandomRotator.cs
+++ Assets/Scripts/RandomRotator.cs
@@ -6,10 +6,10 @@ public class RandomRotator : MonoBehaviour {
 
     public float tumble;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.angularVelocity = Random.insideUnitSphere * tumble;
-	}
+    }
 }
